bc05b42897ed2dc4eb82ca93a57b91ca1ac492af
DanielRavina <danielravina@gmail.com>
2015-12-28 18:12:49 -0800

    add error handling on .filter

--- test/fuzzy.test.js
+++ test/fuzzy.test.js
@@ -64,7 +64,26 @@ describe('fuzzy', function(){
       expect(result).to.equal('<r><e>active r<i><c>e');
     });
   });
+
   describe('.filter', function(){
+    it('should return list untouched when the pattern is undefined', function() {
+      arr = ['aba', 'c', 'cacb']
+      var result = fuzzy.filter(undefined, arr);
+      expect(result).to.include.members(arr)
+    })
+
+    it('should return an empty array when the array is undefined', function() {
+      var result = fuzzy.filter('pattern', undefined);
+      expect(result).to.deep.equal([])
+      expect(result).to.be.empty;
+    })
+
+    it('should return an empty array when the array is empty', function() {
+      var result = fuzzy.filter('pattern', []);
+      expect(result).to.deep.equal([])
+      expect(result).to.be.empty;
+    })
+
     it('should return the index and matching array elements', function(){
       var result = fuzzy.filter('ab', ['aba', 'c', 'cacb']);
       expect(result).to.have.length(2);
